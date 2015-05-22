5beb152237ee91b94dde60d898010529980e10ca
Rafael PacMe <rafy.pachecomendez@gmail.com>
2015-05-22 00:45:07 -0300

    arregladas la mayoria de las librerias (hwcomposer aún erroneo)

---
+++ libgralloc/Android.mk
@@ -0,0 +1,52 @@
+# Copyright (C) 2008 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+LOCAL_PATH := $(call my-dir)
+
+# HAL module implemenation stored in
+# hw/<OVERLAY_HARDWARE_MODULE_ID>.<ro.product.board>.so
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_SHARED_LIBRARIES := liblog libcutils
+
+LOCAL_SRC_FILES := 	\
+	gralloc.cpp 	\
+	framebuffer.cpp \
+	mapper.cpp
+	
+LOCAL_MODULE := gralloc.$(TARGET_BOARD_PLATFORM)
+LOCAL_CFLAGS:= -DLOG_TAG=\"gralloc\"
+
+ifeq ($(BOARD_HTC_3D_SUPPORT),true)
+   LOCAL_CFLAGS += -DHTC_3D_SUPPORT
+endif
+
+ifeq ($(BOARD_NO_PAGE_FLIPPING),true)
+	LOCAL_CFLAGS += -DNO_PAGE_FLIPPING
+endif
+
+ifeq ($(TARGET_USE_PAN_DISPLAY),true)
+LOCAL_CFLAGS += -DUSE_PAN_DISPLAY=1
+endif
+
+ifeq ($(BOARD_USES_BROADCOM_HARDWARE),true)
+	LOCAL_PRELINK_MODULE   := false
+	LOCAL_SHARED_LIBRARIES += libv3d
+	LOCAL_C_INCLUDES       += hardware/broadcom/brcm_usrlib/dag/v3d_library/inc
+	LOCAL_CFLAGS           += -DBCM_HARDWARE -DLCD_PARTIAL_UPDATES_ENABLED=true
+endif
+
+include $(BUILD_SHARED_LIBRARY)
